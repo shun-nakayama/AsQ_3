95bc031825fcd7c9348473762e6fa6db8ff7ba35
Nakayama <nsfgdjpgbdh20@yahoo.co.jp>
2018-03-27 16:53:41 +0900

    Initial Commit

---
+++ AsQ_3/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  AsQ_3
+//
+//  Created by 中山舜 on 2018/03/27.
+//  Copyright © 2018年 ナカヤマ. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
